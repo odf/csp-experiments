68c331ca166420eb2e65bf1b64ea740bde2f35dc
odf <olaf.delgado@gmail.com>
2013-10-08 11:40:58 +1100

    added an example for wrapping streams with channels

---
+++ echo.js
@@ -0,0 +1,52 @@
+var csp = require('./csp')
+
+fromstream = function(stream, outch, statch)
+{
+  stream.on('data', function(chunk) {
+    csp.go(function* () {
+      yield outch.put(chunk);
+    });
+  });
+
+  stream.on('end', function() {
+    if (statch) {
+      csp.go(function* () {
+        yield statch.put({ status: 'end' });
+      });
+    }
+  });
+
+  stream.on('error', function(err) {
+    if (statch) {
+      csp.go(function* () {
+        yield statch.put({ status: 'error', value: new Error(err) });
+      });
+    } else {
+      throw new Error(err);
+    }
+  });
+}
+
+csp.go(function* () {
+  var inch = csp.chan();
+  var statch = csp.chan();
+
+  process.stdin.setEncoding('utf8');
+  fromstream(process.stdin, inch, statch);
+
+  while (true) {
+    var res = yield csp.select([inch, statch]);
+    var ch = res[0];
+    var out = res[1];
+
+    if (ch == statch)
+    {
+      if (out.status == 'error')
+        throw out.value;
+      else if (out.status == 'end')
+        break;
+    } else {
+      console.log(out);
+    }
+  }
+});
