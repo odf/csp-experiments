dcc7678a8af1d24411462bfe8cd99aba58f4f739
odf <olaf.delgado@gmail.com>
2013-11-08 18:26:08 +1100

    lossy.js tests both dropping and sliding buffers

--- examples/lossy.js
+++ examples/lossy.js
@@ -4,15 +4,24 @@ var cc = require('../core');
 var cu = require('../util');
 var cb = require('../buffers');
 
-var ch = cc.chan(new cb.DroppingBuffer(5));
-cc.go(function*() {
-  for (var i = 1; ; ++i) {
-    if (!(yield ch.push(i)))
-      break;
-    if (i % 10 == 0)
-      yield cc.pass();
-  }
-  ch.close();
-});
+var run = function(buffer) {
+  var ch = cc.chan(buffer);
 
-cu.each(console.log, cu.take(60, ch));
+  cc.go(function*() {
+    for (var i = 1; ; ++i) {
+      if (!(yield ch.push(i)))
+        break;
+      if (i % 10 == 0)
+        yield cc.pass();
+    }
+    ch.close();
+  });
+
+  return cu.each(console.log, cu.take(60, ch)).pull();
+};
+
+cc.go(function*() {
+  yield run(new cb.DroppingBuffer(5));
+  console.log();
+  run(new cb.SlidingBuffer(5));
+});
