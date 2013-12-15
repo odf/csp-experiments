e14e8ae8f78fbdd2953c3fcdb147b3c5acffeb13
odf <olaf.delgado@gmail.com>
2013-12-15 17:33:06 +1100

    first steps of establishing a pub/sub system for actions

--- src/actions.js
+++ src/actions.js
@@ -3,37 +3,87 @@
 var cc = require('./core');
 
 
-exports.pass = function(ms) {
+exports.sleep = function(ms) {
   var t;
-  var result = cc.unresolved;
 
-  t = setTimeout(function() {
+  var cancel = function(a) {
     clearTimeout(t);
-    result = cc.resolved();
-  }, ms);
-
-  return function() {
-    return result;
   };
+
+  return new cc.Action({
+    run: function(a) {
+      t = setTimeout(function() {
+        cancel();
+        a.resolve();
+      }, ms);
+    },
+
+    cancel    : cancel,
+    repeatable: true
+  });
 };
 
 
 exports.select = function(actions) {
-  return function() {
-    for (var i = 0; i < actions.length; ++i) {
-      var res = actions[i]();
-      if (cc.isResolved(res))
-        return cc.resolved({ index: i, value: cc.getValue(res) });
-    }
-    return cc.unresolved;
-  }
-};
+  var n = actions.length;
+  var machines = [];
 
-
-exports.constant = function(val) {
-  var result = cc.resolved(val);
-
-  return function() {
-    return result;
+  var cancel = function(self) {
+    for (var i = 0; i < n; ++i)
+      actions[i].unsubscribe(machines[i]);
+    machines = [];
   };
+
+  var runner = function(target, source) {
+    return function*() {
+      try {
+        var val = yield source;
+        cancel();
+        target.resolve(val);
+      } catch (err) {
+        cancel();
+        target.reject(err);
+      }
+    };
+  };
+
+  return new cc.Action({
+    run: function(self) {
+      machines = actions.map(function(a) {
+        return cc.go(runner(self, a));
+      });
+    },
+    cancel    : cancel,
+    repeatable: true
+  });
 };
+
+
+exports.succeed = function(val) {
+  var result = new cc.Action();
+  result.resolve(val);
+  return result;
+};
+
+
+exports.fail = function(val) {
+  var result = new cc.Action();
+  result.reject(new Error(val));
+  return result;
+};
+
+
+cc.go(function*() {
+  var wait = exports.sleep(1000);
+
+  yield wait;
+  console.log(yield exports.succeed(1));
+  yield wait;
+  console.log(yield exports.succeed(2));
+  yield wait;
+  console.log(yield exports.select([
+    exports.succeed(4),
+    exports.succeed(3),
+    exports.sleep(10000)
+  ]));
+});
--- src/core.js
+++ src/core.js
@@ -4,7 +4,6 @@ require('setimmediate');
 
 var RingBuffer = require('./RingBuffer');
 
-
 var schedule = function() {
   var queue = new RingBuffer(100);
   var scheduleFlush = true;
@@ -14,18 +13,18 @@ var schedule = function() {
     var n = queue.count() / 2;
     for (var i = 0; i < n; ++i) {
       var m = queue.read();
-      var s = queue.read();
-      go_(m, s);
+      var d = queue.read();
+      next(m, d);
     }
   };
 
-  return function(machine, step) {
+  return function(machine, data) {
     if (queue.isFull()) {
       var n = Math.floor(queue.capacity() * 1.5);
       queue.resize(n + n % 2); // resize to the next even length
     }
     queue.write(machine);
-    queue.write(step);
+    queue.write(data);
     if (scheduleFlush) {
       setImmediate(flush);
       scheduleFlush = false;
@@ -34,39 +33,98 @@ var schedule = function() {
 }();
 
 
-var go_ = function(machine, step) {
-  while(!step.done) {
-    var res = step.value();
+const UNRESOLVED = 0;
+const RESOLVED   = 1;
+const REJECTED   = 2;
+const IDLE       = 3;
 
-    switch (res[0]) {
-    case 'error':
-      machine.throw(res[1]);
-    case 'park':
-      schedule(machine, step);
-      return;
-    case 'continue':
-      step = machine.next(res[1]);
-      break;
-    }
+
+function Action(options) {
+  this.options = options || {};
+  this.subscribers = [];
+  this.state = IDLE;
+  this.value = undefined;
+};
+
+Action.prototype.publish = function(subscriber) {
+  schedule(subscriber, [this.state, this.value]);
+};
+
+Action.prototype.subscribe = function(machine) {
+  if (this.state == IDLE) {
+    if (typeof this.options.run == 'function')
+      this.options.run(this);
+    this.state = UNRESOLVED;
+  }
+
+  if (this.state == UNRESOLVED)
+    this.subscribers.push(machine);
+  else
+    this.publish(machine);
+}
+
+Action.prototype.unsubscribe = function(machine) {
+  var i = this.subscribers.indexOf(machine);
+  if (i >= 0)
+    this.subscribers.splice(i, 1);
+
+  if (this.subscribers.length == 0
+      && typeof this.options.cancel == 'function') {
+    this.options.cancel(this);
+    this.state = IDLE;
   }
 };
 
+Action.prototype.update = function(state, val) {
+  if (this.state == RESOLVED || this.state == REJECTED)
+    throw new Error("action is already resolved");
 
-exports.unresolved = [ 'park' ];
+  this.state = state;
+  this.value = val;
 
-exports.rejected   = function(err) { return [ 'error', err ]; };
+  var n = this.subscribers.length;
+  var m = this.options.multiplexed ? n : Math.min(n, 1);
 
-exports.resolved   = function(val) { return [ 'continue', val ]; };
+  for (var i = 0; i < m; ++i)
+    this.publish(this.subscribers.shift());
 
-exports.isResolved = function(res) { return res[0] === 'continue'; };
-
-exports.getValue   = function(res) {
-  if (exports.isResolved(res))
-    return res[1];
+  if (this.options.repeatable)
+    this.state = IDLE;
 };
 
-exports.go = function(machine) {
+Action.prototype.resolve = function(val) {
+  this.update(RESOLVED, val);
+};
+
+Action.prototype.reject = function(cause) {
+  this.update(REJECTED, cause);
+};
+
+
+var next = function(machine, data) {
+  var step;
+
+  if (data === undefined)
+    step = machine.next();
+  else if (data[0] == RESOLVED)
+    step = machine.next(data[1]);
+  else
+    step = machine['throw'](data[1]);
+
+  if (!step.done)
+    step.value.subscribe(machine);
+};
+
+
+var go = function(machine) {
   var args = Array.prototype.slice.call(arguments, 1);
-  var gen = machine.apply(undefined, args);
-  go_(gen, gen.next());
+  var m = machine.apply(undefined, args);
+  schedule(m);
+  return m;
+};
+
+
+module.exports = {
+  Action: Action,
+  go    : go
 };
