fb295ae90d74354ad0ce99fe7b398a6a5df9e1a8
odf <olaf.delgado@gmail.com>
2013-12-28 17:38:11 +1100

    inline and rename runNext() in core

--- src/core.js
+++ src/core.js
@@ -4,30 +4,24 @@ var enqueue = require('./scheduler')();
 var deferred = require('./deferred').deferred;
 
 
-var runNext = function(task, step) {
-  var result = step.value;
-
-  if (step.done)
-    task.result.resolve(result);
-  else if (result != null && typeof result.then == 'function')
-    result.then(task.succeed, task.fail);
-  else
-    task.succeed(result);
-};
-
-
 exports.go = function(generator) {
-  var args = Array.prototype.slice.call(arguments, 1);
-  var gen = generator.apply(undefined, args);
-  var task = {
-    result: deferred()
+  var args    = Array.prototype.slice.call(arguments, 1);
+  var gen     = generator.apply(undefined, args);
+  var result  = deferred();
+  var succeed = function(val) { enqueue(function() { use(gen.next(val)); }); };
+  var fail    = function(val) { enqueue(function() { use(gen.throw(val)); }); };
+
+  var use = function(step) {
+    var val = step.value;
+
+    if (step.done)
+      result.resolve(val);
+    else if (val != null && typeof val.then == 'function')
+      val.then(succeed, fail);
+    else
+      succeed(val);
   };
-  task.succeed = function(val) {
-    enqueue(function() { runNext(task, gen.next(val)); });
-  };
-  task.fail = function(cause) {
-    enqueue(function() { runNext(task, gen['throw'](cause)) });
-  };
-  task.succeed();
-  return task.result;
+
+  succeed();
+  return result;
 };
